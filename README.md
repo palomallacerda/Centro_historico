15d3a79ff3a0d4794a631cce4c79c893176a23e2
yrribeiro <ynk9a@outlook.com>
2022-05-29 15:42:18 -0300

    set initial position + left arrow or a key motion + readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Modelagem de um centro histórico: <b>Elevador Lacerda</b>
+Feito com Blender 3.1, Pywavefront, Pyglet e PyOpenGL
+
+<img src="elevador.gif">
+
+## Ações no teclado
+Pressione <i>seta para esquerda</i> ou <i>A</i> para rotacionar o objeto no sentido anti-horário.
+
+
+[@palomallacerda](https://github.com/palomallacerda)
+[@yrribeiro](https://github.com/yrribeiro) | UFAL, 2022
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pyglet
 import pywavefront
 from pywavefront import visualization
 from pyglet.gl import *
+from pyglet.window import key
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
 
 window = pyglet.window.Window(fullscreen=True)
 window.projection = pyglet.window.Projection3D()
@@ -12,10 +16,21 @@ def on_draw():
     window.clear()
     visualization.draw(scene)
 
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.LEFT or symbol == key.A:
+        glRotated(25,0,1,0)
+        glTranslated(25, 0, 0)
 
 if __name__ == "__main__":
-    glEnable(GL_MULTISAMPLE_ARB)
-    glEnable(GL_DEPTH_TEST)
+    glViewport(0, 0, 500,500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+    for _ in range(4):
+        glRotated(25,0,1,0)
+        glTranslated(35, 0, 0)
 
-    glTranslatef(0, 0, -3)
     pyglet.app.run()
